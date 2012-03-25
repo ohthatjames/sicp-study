daa55e8cffc89babcde95e0af2eb7c40e4a05c6c
James Hunt <ohthatjames@gmail.com>
2012-03-25 15:28:12 +0100

    Ex 2.4

---
+++ chapter2/ex2_4.scm
@@ -0,0 +1,15 @@
+; Exercise 2.4.  Here is an alternative procedural representation of pairs. For this representation, verify that (car (cons x y)) yields x for any objects x and y.
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+; What is the corresponding definition of cdr? (Hint: To verify that this works, make use of the substitution model of section 1.1.5.)
+
+(car (cons x y))
+(car (lambda (m) (m x y)))
+((lambda (m) (m x y)) (lambda (p q) p))
+((lambda (p q) p) x y)
+x
